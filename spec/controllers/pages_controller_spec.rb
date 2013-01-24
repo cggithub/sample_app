6034af65f1f831c4daecd0dfd58eb9c90ea022d1
guillermin <clement.guillermin@etud.univ-angers.fr>
2013-01-24 16:03:31 +0100

    Fin Chapitre 3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
 	render_views
 
+	before(:each) do
+		@base_title = "Simple App du Tutoriel Ruby on Rails | "
+	end
+
   describe "GET 'home'" do
 
     it "devrait etre un succes" do
@@ -12,7 +16,7 @@ describe PagesController do
 
 		it "devrait avoir le bon titre" do
 			get 'home'
-			response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
+			response.should have_selector("title", :content => @base_title + "Accueil")
 		end
 
   end
@@ -27,7 +31,7 @@ describe PagesController do
 
 		it "devrait avoir le bon titre" do
 			get 'contact'
-			response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | Contact")
+			response.should have_selector("title", :content => @base_title + "Contact")
 		end
 
   end
@@ -42,9 +46,25 @@ describe PagesController do
 
 		it "devrait avoir le bon titre" do
 			get 'about'
-			response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | A propos")
+			response.should have_selector("title", :content => @base_title + "A propos")
 		end
 
 	end
+
+
+	describe "GET 'help'" do
+
+		it "devrait etre un succes" do
+			get 'help'
+			response.should be_success
+		end
+
+		it "devrait avoir le bon titre" do
+			get 'help'
+			response.should have_selector("title", :content => @base_title + "Aide")
+		end
+
+	end
+
 
 end
